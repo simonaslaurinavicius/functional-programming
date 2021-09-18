5cd34da977f1081f44edfe7d4f18db971d2e3a74
Simonas Laurinavicius <simonas_laurinavicius@outlook.com>
2021-09-18 12:44:22 +0300

    Finish 1 problem set
    Exercises [5,6,7,8] solved.

--- exercises/1/Solutions.hs
+++ exercises/1/Solutions.hs
@@ -7,7 +7,7 @@
   Author: Simonas Laurinavicius
   Study program, course, group: Informatics, 4, 1
   Email: simonas.laurinavicius@mif.stud.vu.lt
-  Last updated: 2021-09-17
+  Last updated: 2021-09-18
 -}
 
 module Solutions where
@@ -78,3 +78,83 @@ nRoots a b c
   | b ^ 2 == 4 * a * c = 1
   | b ^ 2 < 4 * a * c = 0
 
+{-
+  Exercise 5 -
+    define the functions smallerRoot :: Float −> Float −> Float −> Float
+    and largerRoot :: Float −> Float −> Float −> Float, which respectively
+    return the smaller and larger root of a quadratic equation,
+    for the given real coefficients a, b, and c.
+-}
+
+rootHelper :: Float -> Float -> Float -> ((Float, Float) -> Float) -> Float
+rootHelper a b c extrema
+  | nRoots a b c == 0 = error "there are no roots for the coefficients!"
+  | nRoots a b c == 1 = fst (roots a b c)
+  | nRoots a b c == 2 = extrema (roots a b c)
+
+smallerRoot :: Float -> Float -> Float -> Float
+smallerRoot a b c = rootHelper a b c (minimum)
+
+largerRoot :: Float -> Float -> Float -> Float
+largerRoot a b c = rootHelper a b c (maximum)
+
+roots :: Float -> Float -> Float -> (Float, Float)
+roots a b c =
+  (((-b) + sqrt (b ^ 2 - 4 * a * c)) / (2 * a),
+  ((-b) - sqrt (b ^ 2 - 4 * a * c)) / (2 * a))
+
+
+{-
+  Exercise 6 -
+    write a function power2 :: Integer -> Interger that calculates
+    the power of 2 for the given natural number using primitive recursion
+    mechanism.
+-}
+
+power2 :: Integer -> Integer
+power2 n
+  | n == 1 = 2
+  | n > 1 = 2 * power2 (n - 1)
+  | otherwise = 0
+
+{-
+  Exercise 7 -
+    Write a function mult :: Integer −> Integer −> Integer
+    that recursively redefines the multiplication operation
+    by using only addition.
+-}
+
+mult :: Integer -> Integer -> Integer
+mult m n
+  | m == 1 = n
+  | m > 1 = n + mult (m - 1) n
+  | m < 0 = - mult (-m) n
+  | otherwise = 0
+
+{-
+  Exercise 8 -
+    Define a recursive function prod :: Integer −> Integer −> Integer
+    that, for the given numbers m and n,
+    multiplies all the numbers from the range m...n.
+-}
+
+prod :: Integer -> Integer -> Integer
+prod m n
+  | n - m == 0 = n
+  | n - m > 0 = m * prod (m + 1) n
+  | otherwise = error "invalid range, first parameter must be less than second!"
+
+{-
+  redefine factorial function:
+    fact :: Integer -> Integer
+    fact n
+      | n == 0 = 1
+      | n > 0 = fact(n - 1) * n
+
+    as a special case of prod
+-}
+
+fact :: Integer -> Integer
+fact n
+  | n == 0 = 1
+  | n > 0 = prod 1 n
